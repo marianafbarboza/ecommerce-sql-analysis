ff05244f8907254dff1fd29891fb256787e0fdee
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-03-24 20:01:06 -0300

    Update 04_business_metrics.sql

--- sql/04_business_metrics.sql
+++ sql/04_business_metrics.sql
@@ -1,4 +1,5 @@
 
+--Customers with more than one order
 SELECT
 	COUNT(DISTINCT customer_unique_id) AS total_customers,
 	SUM(CASE WHEN orders > 1 THEN 1 ELSE 0 END) AS recurring_customers
@@ -9,4 +10,11 @@ FROM (
 	FROM customers c
 	JOIN orders o ON c.customer_id = o.customer_id
 	GROUP BY c.customer_unique_id
-)
+) t;
+
+--Revenue for delivered orders
+SELECT
+	SUM(op.payment_value) AS receita
+FROM orders o
+JOIN order_payments op ON o.order_id = op.order_id
+WHERE o.order_status = 'delivered';
