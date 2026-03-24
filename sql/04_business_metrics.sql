8678899b79692752addf84d0ec9b19e80d186fb7
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-03-24 19:59:11 -0300

    Update 04_business_metrics.sql

--- sql/04_business_metrics.sql
+++ sql/04_business_metrics.sql
@@ -1,1 +1,12 @@
 
+SELECT
+	COUNT(DISTINCT customer_unique_id) AS total_customers,
+	SUM(CASE WHEN orders > 1 THEN 1 ELSE 0 END) AS recurring_customers
+FROM (
+	SELECT
+		c.customer_unique_id,
+		COUNT(o.order_id) AS orders
+	FROM customers c
+	JOIN orders o ON c.customer_id = o.customer_id
+	GROUP BY c.customer_unique_id
+)
