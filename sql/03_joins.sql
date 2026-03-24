74aae05ba2451044608adbe5b14694aef60f4ced
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-03-24 20:00:59 -0300

    Update 03_joins.sql

--- sql/03_joins.sql
+++ sql/03_joins.sql
@@ -6,14 +6,6 @@ FROM customers c
 LEFT JOIN orders o ON c.customer_id = o.customer_id
 WHERE o.order_id IS NULL;
 
-
---Revenue for delivered orders
-SELECT
-	SUM(op.payment_value) AS receita
-FROM orders o
-JOIN order_payments op ON o.order_id = op.order_id
-WHERE o.order_status = 'delivered';
-
 --Customers by order status
 SELECT
 	o.order_status,
