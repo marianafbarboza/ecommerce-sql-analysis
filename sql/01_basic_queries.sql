8694b5db415167f1bcf92c10da007f64ea413f32
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-03-24 19:34:04 -0300

    Update 01_basic_queries.sql

--- sql/01_basic_queries.sql
+++ sql/01_basic_queries.sql
@@ -1,1 +1,36 @@
+-- Initial queries to better understand the analyzed database
 
+--Top 10 users by payment amount, considering delivery status
+SELECT
+	c.customer_id,
+	SUM(op.payment_value) AS total_payments
+FROM customers c
+JOIN orders o ON c.customer_id  = o.customer_id
+JOIN order_payments op ON o.order_id = op.order_id
+GROUP BY c.customer_id
+ORDER BY total_payments DESC
+LIMIT 10;
+
+--To check the available order statuses
+SELECT DISTINCT order_status
+FROM orders;
+
+--Number of sales above R$2,000
+SELECT COUNT(*) payment_value
+FROM order_payments
+WHERE payment_value > 2000;
+
+--Most commonly used payment methods
+SELECT payment_type,
+COUNT(*) AS payment_total
+FROM order_payments
+GROUP BY payment_type
+ORDER BY payment_total DESC;
+
+--Total number of orders
+SELECT COUNT(*) order_id
+FROM orders;
+
+--Total number of users
+SELECT COUNT(*) AS total_customers
+FROM customers;
