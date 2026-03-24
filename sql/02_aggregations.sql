8a32d82fce30b806878a3655b863c4d4eab41a5d
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-03-24 19:34:57 -0300

    Update 02_aggregations.sql

--- sql/02_aggregations.sql
+++ sql/02_aggregations.sql
@@ -1,1 +1,11 @@
 
+--Top 10 users by payment amount, considering delivery status
+SELECT
+	c.customer_id,
+	SUM(op.payment_value) AS total_payments
+FROM customers c
+JOIN orders o ON c.customer_id  = o.customer_id
+JOIN order_payments op ON o.order_id = op.order_id
+GROUP BY c.customer_id
+ORDER BY total_payments DESC
+LIMIT 10;
