648647ecc7282d29b07b3656d4e3645e91a35439
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-03-24 20:00:17 -0300

    Update 03_joins.sql

--- sql/03_joins.sql
+++ sql/03_joins.sql
@@ -1,38 +1,11 @@
 --Título
 
---Customers with more than one order
-SELECT
-	COUNT(DISTINCT customer_unique_id) AS total_customers,
-	SUM(CASE WHEN orders > 1 THEN 1 ELSE 0 END) AS recurring_customers
-FROM (
-	SELECT
-		c.customer_unique_id,
-		COUNT(o.order_id) AS orders
-	FROM customers c
-	JOIN orders o ON c.customer_id = o.customer_id
-	GROUP BY c.customer_unique_id
-) t;
-
 --Users with no orders
 SELECT COUNT(DISTINCT c.customer_unique_id) AS customers_zero_orders
 FROM customers c
 LEFT JOIN orders o ON c.customer_id = o.customer_id
 WHERE o.order_id IS NULL;
 
---Avegrage orders by month
-SELECT
-	month,
-	AVG(total_orders) AS media_orders
-FROM (
-	SELECT
-		STRFTIME('%Y', order_purchase_timestamp) AS year,
-		STRFTIME('%m', order_purchase_timestamp) AS month,
-		COUNT(*) AS total_orders
-	FROM orders
-	GROUP BY year, month
-) t
-GROUP BY month
-ORDER BY month;
 
 --Revenue for delivered orders
 SELECT
